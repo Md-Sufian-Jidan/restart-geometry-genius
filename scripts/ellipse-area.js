712bbe88a912689316a1c02bd198ea8df4c6dbb0
Md-Sufian-Jidan <jidanjiyaj03@gmail.com>
2025-02-24 15:36:45 +0600

    ellipse calculate done

---
+++ scripts/ellipse-area.js
@@ -0,0 +1,7 @@
+function calculateEllipseArea() {
+    const majorRadius = getInputValueById('ellipse-major');
+    const minorRadius = getInputValueById('ellipse-minor');
+    const area = 3.14 * majorRadius * minorRadius;
+    setInnerTextById('ellipse-area', area);
+
+}
